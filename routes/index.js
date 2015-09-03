16b3293213a0e29b62327660cd0bf8bd40c9a531
Barry Bankhead <bcbankhead@gmail.com>
2015-09-03 17:35:34 -0600

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'nothing to see here, move along :3' });
+});
+
+
+
+module.exports = router;
